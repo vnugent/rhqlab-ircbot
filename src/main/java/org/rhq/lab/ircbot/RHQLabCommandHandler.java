f470726e3e5227141ad7268574ff52493ec621fe
Viet Nguyen <viet.atx@gmail.com>
2014-03-21 14:03:44 -0700

    Handle invalid characters in user nick

--- src/main/java/org/rhq/lab/ircbot/RHQLabCommandHandler.java
+++ src/main/java/org/rhq/lab/ircbot/RHQLabCommandHandler.java
@@ -3,7 +3,6 @@ package org.rhq.lab.ircbot;
 import org.apache.commons.lang3.StringUtils;
 import org.pircbotx.PircBotX;
 import org.pircbotx.hooks.ListenerAdapter;
-import org.pircbotx.hooks.events.MessageEvent;
 import org.pircbotx.hooks.events.PrivateMessageEvent;
 
 import com.redhat.jonlab.cli.CmdLineExecutor;
@@ -12,22 +11,16 @@ import com.redhat.jonlab.cli.CmdLineExecutor;
 public class RHQLabCommandHandler extends ListenerAdapter<PircBotX> {
 	public static final String RHQLAB = "!lab";
 
-//	@Override
-//	public void onMessage(MessageEvent<PircBotX> event) {
-//		try {
-//			processMessage(event);
-//		} catch (Exception e) {
-//			event.respond("Error: " + e.getMessage());
-//		}
-//	}
 	
 	@Override
 	public void onPrivateMessage(PrivateMessageEvent<PircBotX> event) throws Exception {
 		if (StringUtils.isBlank(event.getMessage())) {
 			return;
 		}
+		String userNick = event.getUser().getNick();
+		String safeNick = userNick.replaceAll("[^a-zA-Z0-9.-]", "_");
 		
-		String rhqlabCmd = "-o " + event.getUser().getNick() + " " + event.getMessage();
+		String rhqlabCmd = "-o " + safeNick + " " + event.getMessage();
 	
        	System.out.println(rhqlabCmd);
 
